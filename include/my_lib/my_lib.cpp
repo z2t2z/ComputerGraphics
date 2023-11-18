f792eff662f4e9818f8d2d3a6abdd41077eecd6f
z2t2z <1094696919@qq.com>
2023-11-18 18:07:03 +0000

    1

---
+++ include/my_lib/my_lib.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include <my_lib.h>
+#include "fmt/format.h"
+
+void my_print() {
+    std::cout << "Hello\n";
+    std::cout << "FMT Version:" << FMT_VERSION << "\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "my_lib.h"
+
+int main() {
+    // std::cout << "Hello\n";
+    my_print();
+    return 0;
+}
